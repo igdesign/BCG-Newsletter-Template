d01fc4c463d23e5e8ed45e8f7ddbd97d543270ce
Gregory Daynes <gregdaynes@gmail.com>
2014-04-04 22:51:18 -0700

    reformatting and breaking apart into smaller chunks

---
+++ index_preheader.php
@@ -0,0 +1,17 @@
+<table border="0"
+       cellpadding="0"
+       cellspacing="0"
+       width="728"
+       id="templatePreheader"
+       style="border-collapse: collapse;
+       mso-table-lspace: 0pt;
+       mso-table-rspace: 0pt;
+       -ms-text-size-adjust: 100%;
+       -webkit-text-size-adjust: 100%;
+       background-color: #FFFFFF;
+       border-top: 0;
+       border-bottom: 0;">
+  <tr>
+    <jdoc:include type="modules" name="newsletter-pre-header" style="none" />
+  </tr>
+</table>
