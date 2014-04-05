d6e2c8937e5a33b3db5ceb0c09b83f0c3f62a259
Gregory Daynes <gregdaynes@gmail.com>
2014-04-04 22:51:20 -0700

    reformatting and breaking apart into smaller chunks

---
+++ index_header-leaderboard.php
@@ -0,0 +1,29 @@
+<table border="0"
+       cellpadding="0"
+       cellspacing="0"
+       width="728"
+       id="templateHeader"
+       style="border-collapse: collapse;
+              mso-table-lspace: 0pt;
+              mso-table-rspace: 0pt;
+              -ms-text-size-adjust: 100%;
+              -webkit-text-size-adjust: 100%;
+              background-color: #FFFFFF;
+              border-top: 0;
+              border-bottom: 0;">
+
+
+  <!-- LEADERBOARD -->
+  <tr>
+    <td class="mcnImageContent"
+        valign="top"
+        style="mso-table-lspace: 0pt;
+               mso-table-rspace: 0pt;
+               -ms-text-size-adjust: 100%;
+               -webkit-text-size-adjust: 100%;">
+      <jdoc:include type="modules" name="newsletter-leaderboard" style="none" />
+    </td>
+  </tr>
+  <!-- /LEADERBOARD -->
+
+</table>
