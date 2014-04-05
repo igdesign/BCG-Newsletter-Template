f817ecbea7a0218d0562cf8fa7717564567b0822
Gregory Daynes <gregdaynes@gmail.com>
2014-04-04 22:51:22 -0700

    reformatting and breaking apart into smaller chunks

---
+++ index_header-img.php
@@ -0,0 +1,37 @@
+<table border="0"
+       cellpadding="0"
+       cellspacing="0"
+       width="728"
+       id="templateHeader"
+       style="border-collapse: collapse;
+              mso-table-lspace: 0pt;
+              mso-table-rspace: 0pt;
+              -ms-text-size-adjust: 100%;
+              -webkit-text-size-adjust: 100%;
+              background-color: #FFFFFF;
+              border-top: 0;
+              border-bottom: 0;">
+
+  <tr>
+    <td valign="top"
+        class="mcnImageBlockInner
+        style="mso-table-lspace: 0pt;
+               mso-table-rspace: 0pt;
+               -ms-text-size-adjust: 100%;
+               -webkit-text-size-adjust: 100%;"">
+
+      <img src="<?=$template?>/img/header2.png"
+           align="left"
+           alt=""
+           width="728"
+           class="mcnImage"
+           style="max-width: 728px;
+                  display: inline !important;
+                  vertical-align: bottom;
+                  border: 0;
+                  outline: none;
+                  text-decoration: none;
+                  -ms-interpolation-mode: bicubic;">
+    </td>
+  </tr>
+</table>
