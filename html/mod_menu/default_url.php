52dfd3400e7a6d67c5c0c16b0d5c36b55ed40287
Gregory Daynes <gregdaynes@gmail.com>
2014-05-13 20:22:49 -0700

    not working in Outlook :(

--- html/mod_menu/default_url.php
+++ html/mod_menu/default_url.php
@@ -1,54 +1,96 @@
+<?php defined('_JEXEC') or die; ?>
+
 <?php
-/**
- * @package     Joomla.Site
- * @subpackage  mod_menu
- *
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
+$href = null;
+$text = null;
+$target = null;
+$title = null;
+$options = null;
+$onclick = null;
 
-defined('_JEXEC') or die;
-
-/* include 'templates/newsletter/template_includes.php' ; */
-
-// Note. It is important to remove spaces between elements.
-$class = $item->anchor_css ? 'class="'.$item->anchor_css.' mc-template-link" ' : 'class="'.$class_sfx.'__link  mc-template-link"';
-$title = $item->anchor_title ? 'title="'.$item->anchor_title.'" ' : '';
-if ($item->menu_image)
-	{
-		$item->params->get('menu_text', 1) ?
-		$linktype = '<img src="'.$item->menu_image.'" alt="'.$item->title.'" /><span class="image-title">'.$item->title.'</span> ' :
-		$linktype = '<img src="'.$item->menu_image.'" alt="'.$item->title.'" />';
-}
-else { $linktype = $item->title;
-}
+$linktype = $item->title;
 $flink = $item->flink;
 $flink = JFilterOutput::ampReplace(htmlspecialchars($flink));
 
+$href = 'href="'.$flink.'"';
+$text = $item->title;
 
-$style = 'style="font-family: Arial;font-size: 12px; text-decoration-line: none; font-weight: normal; line-height: 100%; text-align: center; word-wrap: break-word; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;color:#ffffff;"';
-?>
-
-
-
-<td bgcolor="#5d9732" width="1%" align="center" valign="middle" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;" class="mcnShareContentItemContainer">
-
-<?php
 switch ($item->browserNav) :
 	default:
 	case 0:
-?><a <?= $class; ?> <?= $style; ?> href="<?= $flink; ?>" <?= $title; ?> ><?= $linktype; ?></a><?php
 		break;
+
 	case 1:
-		// _blank
-?><a <?= $class; ?> <?= $style; ?> href="<?= $flink; ?>" target="_blank" <?= $title; ?> ><?= $linktype; ?></a><?php
+	  $target = 'target="_blank"';
 		break;
+
 	case 2:
-		// window.open
 		$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,'.$params->get('window_open');
-			?><a <?= $class; ?> <?= $style; ?> href="<?= $flink; ?>" onclick="window.open(this.href,'targetWindow','<?= $options;?>');return false;" <?= $title; ?> ><?= $linktype; ?></a><?php
+		$onclick = 'onclick="window.open(this.href,\'targetWindow\',\''. $options .'\');return false;"';
 		break;
 endswitch;
 ?>
-
- </td>
+<!-- ITEM -->
+<table align="left"
+       border="0"
+       cellpadding="0"
+       cellspacing="0"
+       width="25%">
+  <tbody>
+    <tr>
+      <td valign="top"
+          style="padding-right: 0;
+                 padding-bottom: 0;"
+          class="mcnFollowContentItemContainer">
+        <table border="0"
+               cellpadding="0"
+               cellspacing="0"
+               width="100%"
+               class="mcnFollowContentItem">
+          <tbody>
+            <tr>
+              <td align="left"
+                  valign="middle"
+                  style="padding-top: 0;
+                         padding-right: 0;
+                         padding-bottom: 0;
+                         padding-left: 0;">
+                <table align="left"
+                       border="0"
+                       cellpadding="0"
+                       cellspacing="0"
+                       width="100%">
+                  <tbody>
+                    <tr>
+                      <td align="center"
+                          valign="middle"
+                          height="36"
+                          class="mcnFollowTextContent"
+                          style="padding-left: 0;
+                                 text-align: center;">
+                        <a <?= $href ?>
+                           <?= $target ?>
+                           <?= $options ?>
+                           style="font-family: Arial;
+                                   font-size: 14px;
+                                   text-decoration: none;
+                                   color: #ffffff;
+                                   font-weight: normal;
+                                   text-align: center;
+                                   display: block;"><?= $text ?></a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </tbody>
+</table>
+<!--[if gte mso 6]>
+</td>
+<td align="left" valign="top">
+<![endif]-->
--- html/mod_menu/preheader.php
+++ html/mod_menu/preheader.php
@@ -9,19 +9,78 @@
 
 defined('_JEXEC') or die; ?>
 
+<table border="0"
+       cellpadding="0"
+       cellspacing="0"
+       width="100%"
+       class="mcnFollowBlock">
+
+  <tbody class="mcnFollowBlockOuter">
+    <tr>
+      <td align="center"
+          valign="top"
+          style="padding: 0"
+          class="mcnFollowBlockInner">
+
+        <table border="0"
+               cellpadding="0"
+               cellspacing="0"
+               width="100%"
+               class="mcnFollowContentContainer">
+          <tbody>
+            <tr>
+              <td align="center"
+                  style="padding-left: 0;
+                         padding-right: 0;">
+                <table border="0"
+                       cellpadding="0"
+                       cellspacing="0"
+                       width="100%"
+                       class="mcnFollowContent">
+                  <tbody>
+                    <tr>
+                      <td align="center"
+                          valign="top"
+                          style="padding-top: 0;
+                                 padding-right: 0;
+                                 padding-left: 0;">
+                        <table border="0"
+                               cellpadding="0"
+                               cellspacing="0"
+                               width="100%">
+                          <tbody>
+                            <tr>
+                              <td valign="top">
 
 <?php foreach ($list as $i => &$item) {
-switch ($item->type) {
-	case 'separator':
-	case 'url':
-	case 'component':
-	case 'heading':
-		require JModuleHelper::getLayoutPath('mod_menu', 'preheader_'.$item->type);
-		break;
+        switch ($item->type) {
+        	case 'separator':
+        	case 'url':
+        	case 'component':
+        	case 'heading':
+        		require JModuleHelper::getLayoutPath('mod_menu', 'preheader_'.$item->type);
+        		break;
 
-	default:
-		require JModuleHelper::getLayoutPath('mod_menu', 'preheader_url');
-		break;
-  }
-}
+        	default:
+        		require JModuleHelper::getLayoutPath('mod_menu', 'preheader_url');
+        		break;
+          }
+        }
 ?>
+
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </tbody>
+</table>
