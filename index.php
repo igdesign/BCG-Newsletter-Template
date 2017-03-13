29258884abb74c4612ee571654144ef845ab0595
Gregory Daynes <gregdaynes@gmail.com>
2017-03-13 12:41:46 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ defined('_JEXEC') or die;
 $params = JFactory::getApplication()->getTemplate(true)->params;
 $doc = JFactory::getDocument();
 $template = JURI::base().'templates/'.$this->template;
-$template = 'http://britishcolumbiagolf.org/templates/'.$this->template;
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
           "http: //www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
